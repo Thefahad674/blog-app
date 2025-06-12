c7eb2be8e281891bb7f2dda2ff59670a7ab3ddd4
Thefahad674 <fahadf30511@gmail.com>
2025-06-12 16:26:23 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import News from "./Components/News";
+ 
 
 const App = () => {
   return (
     <div className="container">
       <div className="news-blog-app">
         <News />
+       
       </div>
     </div>
   );
--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Weather from "./Weather";
 import Calendar from "./Calendar";
-import './News.css'
-import userImg from '../assets/images/user.jpg'
+import "./News.css";
+import userImg from "../assets/images/user.jpg";
+import axios from "axios";
+import noImg from '../assets/images/no-image.jpg.png'
+
+const categories = ["general", "world", "business", "technology", "entertainment", "sports", "science", "health", "nation"]
 
 const News = () => {
+  const [headline, setHeadline] = useState();
+  const [news, setNews] = useState([]);
+  const[slectedCategory, setSelectedCategory] = useState('general')
+  const [searchInput, setSearchInput] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  useEffect(() => {
+    const fetchNews = async () => {
+      let url = `https://gnews.io/api/v4/top-headlines?category=${slectedCategory}&lang=en&apikey=59a16a7c6b078b9c55aa876cca859e59`;
+
+      const response = await axios.get(url);
+      const fetchedNews = response.data.articles;
+
+      fetchedNews.forEach((article)=> {
+        if(!article.image) {
+          article.image = noImg
+        }
+      })
+
+      setHeadline(fetchedNews[0]);
+      setNews(fetchedNews.slice(1, 7));
+
+      console.log(fetchedNews[0]);
+    };
+
+    fetchNews();
+  }, [slectedCategory]);
+
+
+  const handleCategoryClick = (e, category) => {
+    e.preventDefault();
+    setSelectedCategory(category)
+  }
+
   return (
     <>
       <div className="news">
-        <header className="news-header"><h1 className="logo">News & Blogs</h1>
-        <div className="search-bar">
-          <form>
-            <input type="text" placeholder="Search News..."/>
-            <button type="submit">
-              <i className="fa-solid fa-magnifying-glass"></i>
-            </button>
-          </form>
-        </div>
+        <header className="news-header">
+          <h1 className="logo">News & Blogs</h1>
+          <div className="search-bar">
+            <form>
+              <input type="text" placeholder="Search News..." />
+              <button type="submit">
+                <i className="fa-solid fa-magnifying-glass"></i>
+              </button>
+            </form>
+          </div>
         </header>
         <div className="news-content">
           <div className="navbar">
@@ -27,33 +66,49 @@ const News = () => {
             <nav className="categories">
               <h1 className="nav-heading">Categories</h1>
               <div className="nav-links">
-                <a href="#" className="nav-link">General</a>
-                <a href="#" className="nav-link">World</a>
-                <a href="#" className="nav-link">Business</a>
-                <a href="#" className="nav-link">Technology</a>
-                <a href="#" className="nav-link">Entertainment</a>
-                <a href="#" className="nav-link">Sports</a>
-                <a href="#" className="nav-link">Science</a>
-                <a href="#" className="nav-link">Health</a>
-                <a href="#" className="nav-link">Nation</a>
-                <a href="#" className="nav-link">Bookmarks
+                {categories.map((category) => (
+
+                  <a key={category} href="#" className="nav-link" onClick={(e)=> handleCategoryClick(e, category)}>
+                  {category}
+                </a>
+                ))}
+               
+                <a href="#" className="nav-link">
+                  Bookmarks
                   <i className="fa-regular fa-bookmark"></i>
                 </a>
               </div>
             </nav>
           </div>
           <div className="news-section">
-            <div className="headline">Headline</div>
-            <div className="news-grid">News Grid</div>
+            {headline && (
+              <div className="headline">
+                <img src={headline.image || noImg} alt={headline.title} />
+                <h2 className="headline-title">
+                  {headline.title}
+                  <i className="fa-regular fa-bookmark bookmark"></i>
+                </h2>
+              </div>
+            )}
+            <div className="news-grid">
+              {news.map((article, index) => (
+                <div key={index} className="news-grid-item">
+                  <img src={article.image || noImg} alt={article.title} />
+                  <h3>
+                    {article.title}
+                    <i className="fa-regular fa-bookmark bookmark"></i>
+                  </h3>
+                </div>
+              ))}
+            </div>
           </div>
           <div className="my-blogs">My Blogs</div>
           <div className="weather-calendar">
-
-          <Weather />
-          <Calendar />
+            <Weather />
+            <Calendar />
           </div>
         </div>
-          <footer className="news-footer">Footer</footer>
+        <footer className="news-footer">Footer</footer>
       </div>
     </>
   );
