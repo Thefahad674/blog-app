346f4a9ec0bb3103beea70e333365f1c400d1cd8
Thefahad674 <fahadf30511@gmail.com>
2025-06-17 17:29:33 +0530

    weather api done

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import './Weather.css'
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
 
 const Weather = () => {
-  return (
-    <div className='weather'>Weather</div>
-  )
-}
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
 
-export default Weather
+  const search = async () => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=Metric&appid=f64434d2dfc11a462cf9904c187cdd6d`;
+
+    const response = await axios.get(url);
+
+    setData(response.data);
+    setLocation('')
+  };
+
+  const handleInputChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  return (
+    <div className="weather">
+      <div className="search">
+        <div className="search-top">
+          <i className="fa-solid fa-location-dot"></i>
+          <div className="location">{data.name}</div>
+        </div>
+        <div className="search-location">
+          <input
+            type="text"
+            placeholder="Enter Location"
+            value={location}
+            onChange={handleInputChange}
+          />
+          <i className="fa-solid fa-magnifying-glass" onClick={search}></i>
+        </div>
+      </div>
+      <div className="weather-data">
+        <i className="bx  bxs-sun"></i>
+        <div className="weather-type">{data.weather ? data.weather[0] : null}</div>
+        <div className="temp">28°</div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
