12d302e7c2998a165bf5d389f7bae1fbd09a54ea
Thefahad674 <fahadf30511@gmail.com>
2025-06-22 17:00:44 +0530

    Blogs saved in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import News from "./Components/News";
 import Blogs from "./Components/Blogs";
- 
 
 const App = () => {
-  const [showNews, setShowNews] = useState(true)
-  const [showBlogs, setShowBlogs] = useState(false)
-  const [blogs, setBlogs] = useState([])
+  const [showNews, setShowNews] = useState(true);
+  const [showBlogs, setShowBlogs] = useState(false);
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    const savedBlogs = JSON.parse(localStorage.getItem("blogs")) || [];
+    setBlogs(savedBlogs);
+  }, []);
 
   const handleCreateBlog = (newBlog) => {
-    setBlogs((prevBlogs) => [...prevBlogs, newBlog ])
-  }
+    setBlogs((prevBlogs) => {
+      const updatedBlogs = [...prevBlogs, newBlog];
+      localStorage.setItem("blogs", JSON.stringify(updatedBlogs));
+      return updatedBlogs;
+    });
+  };
 
   const handleShowBlogs = () => {
-    setShowNews(false)
-    setShowBlogs(true)
-  }
+    setShowNews(false);
+    setShowBlogs(true);
+  };
 
   const handleBackToNews = () => {
-    setShowNews(true)
-    setShowBlogs(false)
-  }
+    setShowNews(true);
+    setShowBlogs(false);
+  };
   return (
     <div className="container">
       <div className="news-blogs-app">
-        {showNews &&  <News onShowBlogs={handleShowBlogs} blogs={blogs} />}
-        {showBlogs &&  <Blogs onBack={handleBackToNews} onCreateBlog={handleCreateBlog} />}
-       
-
+        {showNews && <News onShowBlogs={handleShowBlogs} blogs={blogs} />}
+        {showBlogs && (
+          <Blogs onBack={handleBackToNews} onCreateBlog={handleCreateBlog} />
+        )}
       </div>
     </div>
   );
